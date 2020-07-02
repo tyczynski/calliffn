01157cca4bb946cc3f6bd51a65365b07157d0bc2
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-07-02 21:16:09 +0200

    feat: simplification of jsdoc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 /**
  * Call the argument if it is a function
  *
- * @param {*} fn - argument to be called
- * @param {...*} args - zero or more arguments
- * @returns {*}
+ * @param fn - argument to be called
+ * @param args - zero or more arguments
  */
 module.exports = (fn, ...args) => {
   if (typeof fn === 'function') {
